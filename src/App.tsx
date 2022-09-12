64125e049b84b93d9dacce1e36242ed49b489f33
Eiman Abdelmoneim <eabdelmoneim@gmail.com>
2022-09-11 22:54:22 -0500

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,25 @@ export default function Home() {
         // If the function fails, we can do something here.
         onError={(error) => console.error(error)}
       >
-        Found the Treasure
+        Found the First Treasure
+      </Web3Button>
+      <br></br>
+      <Web3Button
+        // The contract address
+        contractAddress="0xC8642A0c05052Cefe3208A5F697915cF8EAd90E7"
+        // Access the contract itself, perform any action you want on it:
+        action={(contract) => {
+          contract.call("claim", address, 1, 1);
+        }}
+        // Some customization of colors and styling
+        colorMode="light"
+        accentColor="#F213A4"
+        // If the function is successful, we can do something here.
+        onSuccess={(result) => console.log(result)}
+        // If the function fails, we can do something here.
+        onError={(error) => console.error(error)}
+      >
+        Get Scavenger Hunt Finished NFT
       </Web3Button>
     </div>
   );
